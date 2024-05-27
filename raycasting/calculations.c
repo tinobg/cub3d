cc13e48192111c02163b42a6b6224eaf138fe77d
Jennifer UBALDO <jubaldo@made-f0Ar8s2.clusters.42paris.fr>
2024-05-27 20:36:23 +0200

    norm

--- raycasting/calculations.c
+++ raycasting/calculations.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   calculations.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: brjoves <brjoves@student.42.fr>            +#+  +:+       +#+        */
+/*   By: jubaldo <jubaldo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/23 16:37:14 by jubaldo           #+#    #+#             */
-/*   Updated: 2024/05/27 19:52:37 by brjoves          ###   ########.fr       */
+/*   Updated: 2024/05/27 20:28:53 by jubaldo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,21 +40,25 @@ void	calc_step_and_side_dist(t_cub3d *game, t_ray *ray)
 	}
 }
 
-void calc_perp_wall_dist(t_cub3d *game, t_ray *ray) {
-    if (ray->side == 0)
-        ray->perp_wall_dist = (ray->map_x - game->player.pos_x + (1 - ray->step_x) / 2) / ray->ray_dir_x;
-    else
-        ray->perp_wall_dist = (ray->map_y - game->player.pos_y + (1 - ray->step_y) / 2) / ray->ray_dir_y;
+void	calc_perp_wall_dist(t_cub3d *game, t_ray *ray)
+{
+	if (ray->side == 0)
+		ray->perp_wall_dist = (ray->map_x - game->player.pos_x + \
+								(1 - ray->step_x) / 2) / ray->ray_dir_x;
+	else
+		ray->perp_wall_dist = (ray->map_y - game->player.pos_y + \
+								(1 - ray->step_y) / 2) / ray->ray_dir_y;
 }
 
-void calc_line_height(t_cub3d *game, t_ray *ray) {
-    ray->line_height = (int)(game->win_height / ray->perp_wall_dist);
-    ray->draw_start = -ray->line_height / 2 + game->win_height / 2;
-    if (ray->draw_start < 0)
-        ray->draw_start = 0;
-    ray->draw_end = ray->line_height / 2 + game->win_height / 2;
-    if (ray->draw_end >= game->win_height)
-        ray->draw_end = game->win_height - 1;
+void	calc_line_height(t_cub3d *game, t_ray *ray)
+{
+	ray->line_height = (int)(game->win_height / ray->perp_wall_dist);
+	ray->draw_start = -ray->line_height / 2 + game->win_height / 2;
+	if (ray->draw_start < 0)
+		ray->draw_start = 0;
+	ray->draw_end = ray->line_height / 2 + game->win_height / 2;
+	if (ray->draw_end >= game->win_height)
+		ray->draw_end = game->win_height - 1;
 }
 
 void	calc_all(t_cub3d *game, t_ray *ray)
