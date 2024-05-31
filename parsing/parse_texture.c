60fff7587fc50c6ceb534477e50685ae482e622e
Jennifer UBALDO <jubaldo@bess-f2r1s16.clusters.42paris.fr>
2024-05-31 20:03:20 +0200

    ALL LEAKS FIXED!!!!

--- parsing/parse_texture.c
+++ parsing/parse_texture.c
@@ -6,7 +6,7 @@
 /*   By: jubaldo <jubaldo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/18 17:30:14 by jubaldo           #+#    #+#             */
-/*   Updated: 2024/05/25 22:27:10 by jubaldo          ###   ########.fr       */
+/*   Updated: 2024/05/31 20:01:46 by jubaldo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,20 +23,6 @@ static int	get_texture_index(char *id)
 	else if (ft_strncmp(id, "EA", 2) == 0)
 		return (3);
 	return (-1);
-}
-
-static void	load_texture(t_cub3d *game, char *path, int texture_index)
-{
-	game->textures[texture_index].img = mlx_xpm_file_to_image(game->mlx, path, \
-					&game->textures[texture_index].width, \
-					&game->textures[texture_index].height);
-	if (!game->textures[texture_index].img)
-		error_exit(game, "Error: Failed to load texture");
-	game->textures[texture_index].data = (int *)mlx_get_data_addr \
-					(game->textures[texture_index].img, \
-					&game->textures[texture_index].bpp, \
-					&game->textures[texture_index].size_line, \
-					&game->textures[texture_index].endian);
 }
 
 void	parse_textures(t_cub3d *game, char *line)
@@ -56,6 +42,5 @@ void	parse_textures(t_cub3d *game, char *line)
 		free_tokens(tokens);
 		error_exit(game, "Error: Unknown texture identifier");
 	}
-	load_texture(game, tokens[1], texture_index);
 	free_tokens(tokens);
 }
